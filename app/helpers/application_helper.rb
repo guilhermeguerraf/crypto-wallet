1873df8c8b2cba1112e2f7ba32f7887d934cf0fe
Guilherme Guerra de Faria <guigfaria01@gmail.com>
2022-10-18 10:38:37 -0300

    add helper environment_br

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def convert_date_to_brazilian_format(date_to_convert)
         date_to_convert.strftime("%d/%m/%Y")
     end
+
+    def environment_br
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
 end
