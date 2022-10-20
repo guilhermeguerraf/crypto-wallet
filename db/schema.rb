36c4470e8b9ae7d397c64f21bc60159f1b781915
Guilherme Guerra de Faria <guigfaria01@gmail.com>
2022-10-20 10:53:41 -0300

    create mining type crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_14_140852) do
+ActiveRecord::Schema.define(version: 2022_10_20_134903) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
