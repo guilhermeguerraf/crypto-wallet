c94e3b3bb3b2a1af9c2f0014a05f3105db8d738b
Guilherme <guigfaria01@gmail.com>
2022-10-24 14:34:46 -0300

    add i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,24 @@
 module ApplicationHelper
-    def convert_date_to_brazilian_format(date_to_convert)
-        date_to_convert.strftime("%d/%m/%Y")
-    end
+  def convert_date_to_brazilian_format(date_to_convert)
+    date_to_convert.strftime("%d/%m/%Y")
+  end
 
-    def environment_br
-        if Rails.env.development?
-            "Desenvolvimento"
-        elsif Rails.env.production?
-            "Produção"
-        else
-            "Teste"
-        end
+  def environment_to_ptBR(env)
+    case env
+    when "development"
+      "Desenvolvimento"
+    when "production"
+      "Produção"
+    else
+      "Teste"
     end
+  end
+
+  def language_to_ptBR(locale)
+    locale == :en ? "Inglês" : "Português do Brasil"
+  end
+
+  def coursing_now(course)
+    course.nil? ? "Ruby on Rails" : course
+  end
 end
