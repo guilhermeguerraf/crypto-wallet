904880930dc9aaec0952c509a04693d77573b8f4
Guilherme <guigfaria01@gmail.com>
2022-10-25 14:35:00 -0300

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require bootstrap/dist/js/bootstrap
+//= require jquery
+//= require popper.js/dist/popper
